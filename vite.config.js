afbeb9d6a1d01fdb8934dea25816254ad18df524
Dean Johns A E <deanjohns.ae@printersmysore.co.in>
2026-07-27 17:16:17 +0530

    Configure Pages deployment for final repository

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/Namma-Krishi-App/" : "/",
+  base: process.env.GITHUB_ACTIONS ? "/Namma-Krishi-App-Final/" : "/",
   plugins: [react()],
 });
